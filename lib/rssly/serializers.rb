548d46c4ffc54401a7de0540aede43fcda0e3d5a
Nathan Kot <me@nathankot.com>
2014-04-01 18:03:19 +1300

    Two new serializers.

--- lib/rssly/serializers.rb
+++ lib/rssly/serializers.rb
@@ -1,5 +1,6 @@
 module Rssly
   module Serializers
+    # The base serializer class
     class Serializer
       def initialize(collection)
         @collection = collection
@@ -10,7 +11,46 @@ module Rssly
       end
     end
 
-    class HTML < Serializer
+    # Transforms a collection into a text representation
+    class Text < Serializer
+      ARTICLE_TEMPLATE = <<-EOT
+      ====== %{title} ======
+      Seen @ %{published}
+
+      %{summary}
+
+      Read on: %{url}
+
+      --
+
+      EOT
+
+      def perform
+        @collection.reduce('') do |response, article|
+          response + ARTICLE_TEMPLATE % {
+            title: article.title,
+            summary: article.summary,
+            published: article.published,
+            url: article.url
+          }
+        end
+      end
+    end
+
+    # Transforms a collection into a html representation
+    class HTML < Text
+      ARTICLE_TEMPLATE = <<-EOT
+      <article>
+        <h1 class="title">%{title}</h1>
+        <date>%{published}</date>
+
+        <p>%{summary}</p>
+
+        <a class="read more" href="%{url}">
+          Read on.
+        </a>
+      </article>
+      EOT
     end
   end
 end
